45b004470cb0c2497f252e05b849c24fa5bf4ce1
zsn6034 <826776415@qq.com>
2024-01-08 17:31:49 +0800

    初始化

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,15 @@
+/*
+ * @Author: zsn6034 826776415@qq.com
+ * @Date: 2024-01-07 21:10:10
+ * @LastEditors: zsn6034 826776415@qq.com
+ * @LastEditTime: 2024-01-07 21:12:47
+ * @FilePath: /vuepress-demo/docs/.vuepress/config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { defineUserConfig } from 'vuepress';
+
+export default defineUserConfig({
+	lang: 'zh-CN',
+	title: '你好，vuepress！',
+	description: '我的第一个vp站点',
+});
---
+++ docs/README.md
@@ -0,0 +1,9 @@
+---
+lang: zh-CN
+title: 标题啊啊啊
+description: 描述啊啊啊
+---
+
+# Hello VuePress
+
+你好啊
